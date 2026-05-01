0afb2bf39d3073004af2311c753c77f075052a24
RABIUL HOQUE <rabiulofficial7407@gmail.com>
2026-05-01 10:45:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function scrollToProjects() {
-  document.getElementById("projects").scrollIntoView({
-    behavior: "smooth"
-  });
-}
-
-function scrollToContact() {
-  document.getElementById("contact").scrollIntoView({
-    behavior: "smooth"
-  });
-}
-
-function openProject(name) {
-  alert(name + " project coming soon!");
-}
+console.log("Portfolio Loaded 🚀");
